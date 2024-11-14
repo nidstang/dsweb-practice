2c422894c3ca837df7861e9e756e67b2bb804b89
Pablo Fernández Franco <pableudo@gmail.com>
2024-11-14 20:01:03 +0000

    update with class 5

--- build.js
+++ build.js
@@ -6,14 +6,63 @@ register(StyleDictionary, {
     withSDBuiltins: false,
 });
 
+// load
 const loader = ThemesLoader(StyleDictionary);
 
-// esto es un comentario
-
+// /src/main.css
 async function main () {
-    console.log('Hola Pablo');
-    console.log('adios');
-    sdfsdf
+    const holder = await loader.load("/tokens");
+
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'build/global/variables.css',
+                        format: 'css/variables'
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'size/pxToRem'
+                ]
+            },
+
+            // android: {
+            //     transforms: [
+            //         'name/snake'
+            //     ],
+            //     files: [
+            //         {
+            //             destination: 'build/android/global/resources.xml',
+            //             format: 'android/resources'
+            //         }
+            //     ]
+            // }
+        }
+    };
+
+    holder.getThemeByName('global').addConfig(config).build();
 }
 
 main();
+
+// esto es un comentario
+// string, number, boolean, object, array
+
+// const name = "Pablo";
+
+// const person = {
+//     isAmazing: true,
+//     name: "Pablo",
+//     lastname: "Fernandez",
+//     age: 32,
+//     list: ["test", 2],
+// }
+
+// console.log(person);
+// console.log(person.age);
+// console.log(person.isAmazing);
+// console.log(person.list);
